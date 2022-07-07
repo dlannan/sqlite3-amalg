69542af6ef6c26ff263e4c5dcb2459656de471ac
dlannan <dlannan@internode.on.net>
2022-07-07 22:55:48 +0930

    manual compile call

--- build-sqlite3.sh
+++ build-sqlite3.sh
@@ -5,8 +5,8 @@ PLATFORM=$1
 echo "Building for platform ${PLATFORM}"
 
 if [ "${PLATFORM}" = "win64" ]; then
-	cl /c /EHsc -I./src src/shell.c src/sqlite3.c -Fesqlite3.obj
-    lib /out:libsqlite3.lib sqlite3.obj 
+	cl /c /EHsc -I./src src/shell.c src/sqlite3.c -Felibsqlite3.obj
+    lib libsqlite3.obj 
 elif [ "${PLATFORM}" = "linux" ]; then
     gcc -c -o sqlite3.o -I./src src/shell.c src/sqlite3.c -lpthread -ldl -lm 
     ar rcs libsqlite3.a sqlite3.o
