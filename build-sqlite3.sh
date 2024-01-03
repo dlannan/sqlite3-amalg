dc2a4085329a8eac5776caa7ccc26061376a355c
David Lannan <dlannan@internode.on.net>
2024-01-03 19:21:10 +1030

    Update build-sqlite3.sh

--- build-sqlite3.sh
+++ build-sqlite3.sh
@@ -17,7 +17,8 @@ elif [ "${PLATFORM}" = "ios64" ]; then
     gcc -c -I./src src/shell.c src/sqlite3.c -lpthread -ldl -lm -arch arm64
     ar rcs libsqlite3.a sqlite3.o shell.o
 elif [ "${PLATFORM}" = "android" ]; then
-    gcc -c -I./src src/shell.c src/sqlite3.c -lpthread -ldl -lm -march=armv7
+    sudo apt install crossbuild-essential-armhf crossbuild-essential-arm64
+    arm-linux-gnueabi-gcc -c -I./src src/shell.c src/sqlite3.c -lpthread -ldl -lm
     ar rcs libsqlite3.a sqlite3.o shell.o    
 fi
 
