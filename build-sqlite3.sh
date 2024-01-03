18f72d91607deac64a4b72dde6adf9ca3d5bd695
David Lannan <dlannan@internode.on.net>
2024-01-04 09:18:24 +1030

    Update build-sqlite3.sh

--- build-sqlite3.sh
+++ build-sqlite3.sh
@@ -17,7 +17,7 @@ elif [ "${PLATFORM}" = "ios64" ]; then
     gcc -c -I./src src/shell.c src/sqlite3.c -lpthread -ldl -lm -arch arm64
     ar rcs libsqlite3.a sqlite3.o shell.o
 elif [ "${PLATFORM}" = "android" ]; then
-    aarch64-linux-gnu-gcc -c -I./src src/shell.c src/sqlite3.c -lpthread -ldl -lm -fPIC 
+    aarch64-linux-gnu-gcc -c -I./src src/shell.c src/sqlite3.c -lpthread -ldl -lm -fPIC -static
     ar rcs libsqlite3.a sqlite3.o shell.o    
 fi
 
