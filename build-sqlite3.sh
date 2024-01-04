00a7b6a74001dbbc5c319b61646c3a799619de11
David Lannan <dlannan@internode.on.net>
2024-01-04 16:28:35 +1030

    Update build-sqlite3.sh

--- build-sqlite3.sh
+++ build-sqlite3.sh
@@ -14,15 +14,15 @@ elif [ "${PLATFORM}" = "macosx" ]; then
     gcc -c -I./src src/shell.c src/sqlite3.c -lpthread -ldl -lm 
     ar rcs libsqlite3.a sqlite3.o shell.o
 elif [ "${PLATFORM}" = "ios64" ]; then
-    gcc -c -I./src src/shell.c src/sqlite3.c -lpthread -ldl -lm -arch aarch64
+    gcc -c -I./src src/shell.c src/sqlite3.c -lpthread -ldl -lm -arch arm64
     ar rcs libsqlite3.a sqlite3.o shell.o
 elif [ "${PLATFORM}" = "android" ]; then
     export PATH="android-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
     export PATH="android-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/:$PATH"
-    aarch64-linux-android30-clang -c -I./src -Iandroid-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include src/shell.c src/sqlite3.c -D__ANDROID__ -lpthread -ldl -lm -fPIC -static -arch aarch64
+    aarch64-linux-android30-clang -c -I./src -Iandroid-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include src/shell.c src/sqlite3.c -D__ANDROID__ -lpthread -ldl -lm -fPIC -static -arch arm64
     llvm-ar rcs libsqlite3.a sqlite3.o shell.o    
     rm sqlite3.o shell.o
-    armv7a-linux-androideabi30-clang -c -I./src src/shell.c -Iandroid-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include src/sqlite3.c -D__ANDROID__ -target armv7a-none-linux-android19 -lpthread -ldl -lm -fPIC -static -arch armv7a
+    armv7a-linux-androideabi30-clang -c -I./src src/shell.c -Iandroid-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include src/sqlite3.c -D__ANDROID__ -target armv7a-none-linux-android19 -lpthread -ldl -lm -fPIC -static -arch armv7
     llvm-ar rcs libsqlite3-armv7.a sqlite3.o shell.o    
 fi
 
