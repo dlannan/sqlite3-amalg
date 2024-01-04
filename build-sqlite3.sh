df9e04c7cfbd24da6788e9f8bdf59a3ab54df1e1
David Lannan <dlannan@internode.on.net>
2024-01-04 17:56:26 +1030

    Update build-sqlite3.sh

--- build-sqlite3.sh
+++ build-sqlite3.sh
@@ -23,7 +23,7 @@ elif [ "${PLATFORM}" = "android" ]; then
     aarch64-linux-android30-clang -c -I./src -Iandroid-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include src/shell.c src/sqlite3.c -D__ANDROID__ -DminSdkVersion=19 -lpthread -ldl -lm -fPIC -static
     llvm-ar rcs libsqlite3.a sqlite3.o shell.o    
     rm sqlite3.o shell.o
-    armv7a-linux-androideabi30-clang -c -I./src -Iandroid-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include src/shell.c src/sqlite3.c -D__ANDROID__ -DminSdkVersion=19 -lpthread -ldl -lm -fPIC -static
+    armv7a-linux-androideabi30-clang -c -I./src -Iandroid-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include src/shell.c src/sqlite3.c -D__ANDROID__ --api 21 -lpthread -ldl -lm -fPIC -static
     llvm-ar rcs libsqlite3-armv7.a sqlite3.o shell.o    
 fi
 
