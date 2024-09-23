177385529388fdc25a760a5ec20aab9b66519f61
David Lannan <dlannan68@gmail.com>
2024-09-23 23:18:28 +0930

    Update mmap.c

--- src/mmap.c
+++ src/mmap.c
@@ -4,10 +4,10 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-#ifdef __ANDROID__
-extern void*  __mmap2(void *, size_t, int, int, int, off_t);
-void *mmap64(void *addr, size_t length, int prot, int flags, int fd, off64_t offset)
-{
-    return __mmap2(addr, length, prot, flags, fd, offset >> 12);
-}
-#endif
+// #ifdef __ANDROID__
+// extern void*  __mmap2(void *, size_t, int, int, int, off_t);
+// void *mmap64(void *addr, size_t length, int prot, int flags, int fd, off64_t offset)
+// {
+//     return __mmap2(addr, length, prot, flags, fd, offset >> 12);
+// }
+// #endif
