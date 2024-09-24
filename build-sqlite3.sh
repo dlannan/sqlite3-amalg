f17c236f30c6badb9dfdcea927bc8134e9eaa179
David Lannan <dlannan68@gmail.com>
2024-09-24 18:09:43 +0930

    Update build-sqlite3.sh

--- build-sqlite3.sh
+++ build-sqlite3.sh
@@ -20,7 +20,7 @@ elif [ "${PLATFORM}" = "android" ]; then
     export PATH="android-ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
     export PATH="android-ndk/toolchains/llvm/prebuilt/linux-x86_64/:$PATH"
     
-    aarch64-linux-android19-clang++ -c -I./src -Iandroid-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include src/shell.c src/sqlite3.c -D__ANDROID__ -D__ANDROID_MIN_SDK_VERSION__=19 -lpthread -lm -fPIC -static
+    aarch64-linux-android21-clang++ -c -I./src -Iandroid-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include src/shell.c src/sqlite3.c -D__ANDROID__ -D__ANDROID_MIN_SDK_VERSION__=21 -lpthread -lm -fPIC -static
     llvm-ar rcs libsqlite3.a sqlite3.o shell.o    
     rm sqlite3.o shell.o
     armv7a-linux-androideabi19-clang++ -c -I./src -Iandroid-ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include src/shell.c src/sqlite3.c src/mmap.c -D__ANDROID__ -D__ANDROID_MIN_SDK_VERSION__=19 -lpthread -lm -fPIC -static
